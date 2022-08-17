7fc8c42074a6edbb3bd58526de7a2e36026291ff
Issei Saiki <i.saiki@aster-ai.com>
2022-08-17 15:36:35 +0900

    JSで画像処理(グレースケール化)

--- static/js/image.js
+++ static/js/image.js
@@ -2,6 +2,9 @@ window.addEventListener('DOMContentLoaded', function() {
     showImages();
 });
 
+let portionData = {};
+let flag = true;
+
 function showImages() {
     const divImage = document.getElementById("divImage")
     const filename = document.getElementById("fileName").innerHTML
@@ -9,11 +12,63 @@ function showImages() {
         const res = await fetch(`http://localhost:8080/images?filename=${filename}`);
         const resObj = await res.json();
         divImage.style.width = resObj["width"] + "px";
+        // 画像を表示
         for (const elem of resObj["images"]) {
             const imageElement = document.createElement("img")
             imageElement.src = "data:image/png;base64," + elem["code"]
+            imageElement.setAttribute("id", elem["id"])
             divImage.appendChild(imageElement);
+        }
+        // 抽選部位データをセット
+        for (const elem of resObj["portions"]) {
+            portionData[elem["pId"]] = {
+                "userId": elem["userId"], 
+                "familyName": elem["familyName"],
+                "firstName": elem["firstName"],
+                "data": elem["date"],
+            }
+        }
+        // グレースケール化の関数を画像ロード時に実行
+        for (const key in portionData) {
+            const img = document.getElementById(key);
+            img.onload = function(e) {
+                if (!img.classList.contains("gray")) {
+                    fillGray(img);
+                    img.setAttribute("class", "gray")
+                }
+            }
         }
     }
     callApi();
+}
+
+function fillGray(img) {
+    const width = img.width;
+    const height = img.height;
+
+    const testCanvas = document.getElementById("testCanvas");
+    testCanvas.style.width = width + "px";
+    testCanvas.style.height = height + "px";
+    // const canvas = document.createElement("canvas");
+    // canvas.style.width = width
+    // canvas.style.height = height
+    // console.log(canvas);
+    const context = testCanvas.getContext("2d");
+    context.drawImage(img, 0, 0);
+
+    const imgData = context.getImageData(0, 0, width, height);
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            var pix = (i*width + j) * 4;
+            var gray = 0.299 * imgData.data[pix] + 0.587 * imgData.data[pix+1] + 0.114 * imgData.data[pix+2];
+            for (var k = 0; k < 3; k++) {
+                imgData.data[pix+k] = gray;
+           }
+        }
+    }
+    context.putImageData(imgData, 0, 0)
+
+    url = testCanvas.toDataURL();
+    img.src = url;
 }
