04482aebea836b47c9435109acb854edbe8637da
Issei Saiki <i.saiki@aster-ai.com>
2022-08-24 10:34:53 +0900

    新規登録：出品者を選んだ場合に抽選口数の欄を無効化する

--- static/js/validate.js
+++ static/js/validate.js
@@ -3,23 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const requiredElems = document.querySelectorAll('.required');
     const errorClassName = 'error';
 
+    // 未入力の欄に警告エラーを表示
     const createError = (elem, errorMessage) => {
-        //span 要素を生成
         const errorSpan = document.createElement('span');
-        //エラー用のクラスを追加（設定）
         errorSpan.classList.add(errorClassName);
-        //aria-live 属性を設定
         errorSpan.setAttribute('aria-live', 'polite');
-        //引数に指定されたエラーメッセージを設定
         errorSpan.textContent = errorMessage;
-        // テキストを赤色にするcssを設定
         errorSpan.style.color = "#dc3545";
-        //elem の親要素の子要素として追加
         elem.parentNode.appendChild(errorSpan);
     }    
 
     theForm.addEventListener("submit", (e) => {
-    //エラーを表示する要素を全て取得して削除（初期化）
+    //初期化
       const errorElems = theForm.querySelectorAll('.' + errorClassName);
       errorElems.forEach( (elem) => {
         elem.remove(); 
@@ -27,9 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //.required を指定した要素を検証
     requiredElems.forEach( (elem) => {
-        //値（value プロパティ）の前後の空白文字を削除
-        const elemValue = elem.value.trim(); 
-        //値が空の場合はエラーを表示してフォームの送信を中止
+        const elemValue = elem.value.trim(); e
         if(elemValue.length === 0) {
             createError(elem, '入力は必須です');
             e.preventDefault();
@@ -38,7 +31,16 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 });
 
-// const selectUserType = document.getElementById("selectUserType")
-// selectUserType.addEventListener("change", () => {
-//     if selectUserType.value == 
-// })
+var lotteryUnits = 0;
+const selectUserType = document.getElementById("selectUserType")
+const inputLotteryUnits = document.getElementById("inputLotteryUnits")
+selectUserType.addEventListener("change", (e) => {
+    if (selectUserType.value == "sellers") {
+      inputLotteryUnits.disabled = true;
+      inputLotteryUnits.value = "";
+    } else {
+      inputLotteryUnits.disabled = false;
+      inputLotteryUnits.value = "0";
+    }
+    e.preventDefault();
+});
